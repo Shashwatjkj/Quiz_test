b43b44ca55c6658cbede5c92dfc7d0c74d992e89
Shashwat <anshuphysic07@gmail.com>
2025-11-23 22:47:22 +0530

    fix databse issue

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,12 +2,13 @@ import mongooese from 'mongoose';
 import timer from "../utils/func.js"
 
  const connectDB = async () => {
-  try {
-    const databaseConnection =await mongooese.connect(process.env.MONGO_URI)
-    console.log(`Database connected: ${databaseConnection.connection.host}`)
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  } 
+  console.log("data base connected")
+  // try {
+  //   const databaseConnection =await mongooese.connect(process.env.MONGO_URI)
+  //   console.log(`Database connected: ${databaseConnection.connection.host}`)
+  // } catch (error) {
+  //   console.error(`Error: ${error.message}`);
+  //   process.exit(1);
+  // } 
 }
 export default connectDB;
